a82a22019861181b0685d8d9aa43fc698612361b
nicoplata <nicomplata@gmail.com>
2024-01-25 10:28:59 -0300

    version1.4

--- js/main.js
+++ js/main.js
@@ -95,36 +95,3 @@ function scrollFunction() {
 }
 
 /* ---------------------------- */
-
-
-
-
-
-/* let currentIndex = 0;
-
-function showSlide(index) {
-    const carousel = document.querySelector('.carousel');
-    const carouselItems = document.querySelectorAll('.carouselItem');
-    const totalItems = carouselItems.length;
-
-    if (index < 0) {
-        currentIndex = totalItems - 1;
-    } else if (index >= totalItems) {
-        currentIndex = 0;
-    } else {
-        currentIndex = index;
-    }
-
-    const offset = -currentIndex * 100 + '%';
-    carousel.style.transform = 'translateX(' + offset + ')';
-}
-
-function nextSlide() {
-    showSlide(currentIndex + 1);
-}
-
-function prevSlide() {
-    showSlide(currentIndex - 1);
-}
-
-setInterval(nextSlide, 3000); */
